1240c957f06789ad48df35c900ef2083e716e7d1
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-11-24 20:41:36 +0800

    [init] create project from xcode 10.3.

---
+++ Ubike/Ubike/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Ubike
+//
+//  Created by kidnapper on 2019/11/24.
+//  Copyright © 2019 kidnapper. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ubike/Ubike/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Ubike
+//
+//  Created by kidnapper on 2019/11/24.
+//  Copyright © 2019 kidnapper. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
